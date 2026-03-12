26997662d54ff0e9b27f7be8d61fed70581a52d5
Jordy <me@jordanlambrecht.com>
2026-03-12 12:35:14 -0500

    chore: switched to alias paths

--- src/lib/__tests__/totp.test.ts
+++ src/lib/__tests__/totp.test.ts
@@ -8,7 +8,7 @@ import {
   hashBackupCode,
   verifyAndConsumeBackupCode,
   verifyTotpCode,
-} from "../totp"
+} from "@/lib/totp"
 
 // ---------------------------------------------------------------------------
 // generateTotpSecret
